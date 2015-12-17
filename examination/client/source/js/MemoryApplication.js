a0f4c793d1f585ae0e45cf655da9cbd80f7ba9e5
Oskar Emilsson <oskaremilsson@gmail.com>
2015-12-17 15:01:52 +0100

    Adding reset

--- examination/client/source/js/MemoryApplication.js
+++ examination/client/source/js/MemoryApplication.js
@@ -7,6 +7,7 @@ function MemoryApplication(options) {
 
     this.settingsOpen = false;
     this.game = undefined;
+    this.boardSize = [4, 4];
 }
 
 MemoryApplication.prototype = Object.create(BasicWindow.prototype);
@@ -26,31 +27,65 @@ MemoryApplication.prototype.print = function() {
     this.element.classList.add("memory-app");
 
     var menu = this.element.querySelector(".window-menu");
-    var template = document.querySelector("#template-window-menu-alternative").content.cloneNode(true);
-    template.querySelector(".menu-alternative").appendChild(document.createTextNode("Settings"));
-    menu.appendChild(template);
+    var alt1 = document.querySelector("#template-window-menu-alternative").content.cloneNode(true);
+    alt1.querySelector(".menu-alternative").appendChild(document.createTextNode("New Game"));
+
+    var alt2 = document.querySelector("#template-window-menu-alternative").content.cloneNode(true);
+    alt2.querySelector(".menu-alternative").appendChild(document.createTextNode("Settings"));
+
+    menu.appendChild(alt1);
+    menu.appendChild(alt2);
 };
 
 MemoryApplication.prototype.menuClicked = function(event) {
     var target;
     if (event.target.tagName.toLowerCase() === "a") {
-        target = event.target.textContent;
+        target = event.target.textContent.toLowerCase();
     }
 
     if (target) {
-        if (!this.settingsOpen) {
-            var template = document.querySelector("#template-settings").content.cloneNode(true);
-            template.querySelector(".settings").classList.add("memory-settings");
+        switch (target) {
+            case "settings": {
+                this.menuSettings();
+                break;
+            }
+            case "new game": {
+                if (this.settingsOpen) {
+                    this.settingsOpen = false;
+                }
+                this.restart();
+                break;
+            }
+        }
+    }
+};
 
-            template = this.addSettings(template);
-            this.element.querySelector(".window-content").appendChild(template);
-            this.settingsOpen = true;
-        }
-        else {
-            var settings = document.querySelector(".settings-wrapper");
-            this.element.querySelector(".window-content").removeChild(settings);
-            this.settingsOpen = false;
-        }
+MemoryApplication.prototype.restart = function(value) {
+    if (value) {
+        this.boardSize = value.split("x");
+    }
+    var y = this.boardSize[1];
+    var x = this.boardSize[0];
+    this.clearContent();
+
+    this.game.removeEvents();
+    this.game = new MemoryGame(this.element.querySelector(".window-content"), x, y);
+    this.game.init();
+};
+
+MemoryApplication.prototype.menuSettings = function() {
+    if (!this.settingsOpen) {
+        var template = document.querySelector("#template-settings").content.cloneNode(true);
+        template.querySelector(".settings").classList.add("memory-settings");
+
+        template = this.addSettings(template);
+        this.element.querySelector(".window-content").appendChild(template);
+        this.settingsOpen = true;
+    }
+    else {
+        var settings = document.querySelector(".settings-wrapper");
+        this.element.querySelector(".window-content").removeChild(settings);
+        this.settingsOpen = false;
     }
 };
 
@@ -64,15 +99,7 @@ MemoryApplication.prototype.addSettings = function(element) {
 
 MemoryApplication.prototype.saveSettings = function() {
     var value = this.element.querySelector("select[name='board-size']").value;
-    value = value.split("x");
-    var y = value[1];
-    var x = value[0];
-
-    this.clearContent();
-
-    this.game.removeEvents();
-    this.game = new MemoryGame(this.element.querySelector(".window-content"), x, y);
-    this.game.init();
+    this.restart(value);
     this.settingsOpen = false;
 };
 
